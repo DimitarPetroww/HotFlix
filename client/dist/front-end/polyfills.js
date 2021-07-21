fc6f9f1ffc0d286e39c5744984760f7580b7ff26
Dimitar.Petroww <Mitko66692@abv.bg>
2021-07-21 20:17:59 +0300

    update

--- client/dist/front-end/polyfills.js
+++ client/dist/front-end/polyfills.js
@@ -7,7 +7,7 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\pc\Desktop\SoftUni\asd\client\src\polyfills.ts */"hN/g");
+module.exports = __webpack_require__(/*! C:\Users\pc\Desktop\HotFlix git\HotFlix\client\src\polyfills.ts */"hN/g");
 
 
 /***/ }),
